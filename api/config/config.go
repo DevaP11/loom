ce38f0d8e678165e6a6b1c78505c7bf525c7f60f
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2026-01-06 13:49:05 +0530

    Update

---
+++ api/config/config.go
@@ -0,0 +1,8 @@
+package config
+
+import "fmt"
+
+func HelloWorld(amount float64) {
+	result := "result"
+	fmt.Printf("%.2f converted to USD is %.2f\n", amount, result)
+}
--- api/route.go
+++ api/route.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"loom.app/config"
 )
 
 func main() {
@@ -10,7 +12,8 @@ func main() {
 	// API routes
 	api := router.Group("/api")
 	{
-		api.GET("/hello", func(c *gin.Context) {
+		api.GET("/config", func(c *gin.Context) {
+			config.HelloWorld(200)
 			c.JSON(200, gin.H{
 				"message": "Hello from the Go API!",
 			})
