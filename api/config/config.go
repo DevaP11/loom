ac47ec4120ed74b6de41f9ba012065410fc4c288
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2026-01-07 12:24:39 +0530

    setup badger

--- api/config/config.go
+++ api/config/config.go
@@ -2,11 +2,22 @@ package config
 
 import (
 	"fmt"
+	"log"
 
+	badger "github.com/dgraph-io/badger/v4"
 	"loom.app/models"
 )
 
 func CreateProjectItem(project models.CreateProject) string {
+	db, err := badger.Open(badger.DefaultOptions("./badger"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	fmt.Printf("Badger Database Loaded\n")
+
 	fmt.Printf("Project name is %s \n & Project Get Config Url is %s \n", project.Name, project.GetConfigUrl)
 	return "Success"
 }
